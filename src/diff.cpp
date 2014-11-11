c162bb4e0a3b0189720ea7b9031116d4b5b26290
Kevin Ryczko <kevinryczko@me.com>
2014-11-11 18:40:15 -0500

    small change in logic for calculations

--- src/diff.cpp
+++ src/diff.cpp
@@ -27,13 +27,15 @@ void diff3D(Storage& s) {
         output << n << "  ";
     }
     output << "\n";
-    for (int i = 0; i < f1.data.size(); i ++) {
-        if (s.operation == "-") {
-            output << abs(f1.data[i] - f2.data[i]) << "  ";
-            if ((i + 1) % 5 == 0) {
-                output << "\n";
-            }
-        } else if (s.operation == "+") {
+    if (s.operation == "-") {
+        for (int i = 0; i < f1.data.size(); i ++) {
+                output << abs(f1.data[i] - f2.data[i]) << "  ";
+                if ((i + 1) % 5 == 0) {
+                    output << "\n";
+                }
+        }
+    } else if (s.operation == "+") {
+        for (int i = 0; i < f1.data.size(); i ++) {
             output << f1.data[i] + f2.data[i] << "  ";
             if ((i + 1) % 5 == 0) {
                 output << "\n";
